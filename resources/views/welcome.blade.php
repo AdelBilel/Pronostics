771771015ba8d6724d863b97818ee46003f63143
younesbenmammar <younescomptepro@gmail.com>
2018-04-09 14:23:38 +0200

    Page d'accueil exemple

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,93 +8,107 @@
 						<!-- Featured Post -->
 							<article class="post featured">
 								<header class="major">
-									<span class="date">April 25, 2017</span>
-									<h2><a href="#">And this is a<br />
-									massive headline</a></h2>
-									<p>Aenean ornare velit lacus varius enim ullamcorper proin aliquam<br />
-									facilisis ante sed etiam magna interdum congue. Lorem ipsum dolor<br />
-									amet nullam sed etiam veroeros.</p>
+
+									<h2><a href="#">Bienvenue Sur <br />
+									PronosDTA</a></h2>
+										<p>
+											Tous es jours vous trouverez les match de la coupe du monde<br>
+											Envie de Gagner des point et etre premier du classement ? <br>
+											Alors fait vos paris et si vous avez le bon prono vous gagnez 3 point<br>
+											A VOUS DE JOUEZ !!!!
+											
+
+
+
+										</p>
 								</header>
-								<a href="#" class="image main"><img src="images/pic01.jpg" alt="" /></a>
+								<a href="#" class="image main"><img src="images/equipe.jpg" alt="" /></a>
 								<ul class="actions">
-									<li><a href="#" class="button big">Full Story</a></li>
+									<li><button class="button big">C'est parti !!!</a></li>
 								</ul>
 							</article>
 
 						<!-- Posts -->
 							<section class="posts">
+							
 								<article>
+							
 									<header>
-										<span class="date">April 24, 2017</span>
-										<h2><a href="#">Sed magna<br />
-										ipsum faucibus</a></h2>
+							
+										<span class="date">Match du jour</span>
+							
+										<br>
+										
+							
+										<h1>Maroc vs Portugal</h1>
+							
 									</header>
-									<a href="#" class="image fit"><img src="images/pic02.jpg" alt="" /></a>
-									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Full Story</a></li>
-									</ul>
+							
+									<a href="#" class="image fit"><img src="images/mvsp.png" alt="" /></a>
+									
+									<p>Pariez sur ce match ? </p>
+									
+									<br>
+									
+										<form >
+											
+												<label for="equipe1" class="col-md-4 control-label">Score Equipe 1 </label>
+
+												
+											<input type="text" name="equipe1" placeholder=" Entrez le score de l'equipe 1 "> 
+											
+											<br>
+
+
+											<label for="equipe1" class="col-md-4 control-label">Score Equipe 2 </label>
+
+											<input type="text" name="equipe1" placeholder=" Entrez le score de l'equipe 2 "> 
+
+								
+											<div class="col-xs-6">
+													<div class="text-center"><button class="btn btn-primary btn-sx" type="button">Confirm</button></div>
+												</div>
+										</form>
+
+									
 								</article>
+								
+								
 								<article>
-									<header>
-										<span class="date">April 22, 2017</span>
-										<h2><a href="#">Primis eget<br />
-										imperdiet lorem</a></h2>
-									</header>
-									<a href="#" class="image fit"><img src="images/pic03.jpg" alt="" /></a>
-									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Full Story</a></li>
-									</ul>
-								</article>
-								<article>
-									<header>
-										<span class="date">April 18, 2017</span>
-										<h2><a href="#">Ante mattis<br />
-										interdum dolor</a></h2>
-									</header>
-									<a href="#" class="image fit"><img src="images/pic04.jpg" alt="" /></a>
-									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Full Story</a></li>
-									</ul>
-								</article>
-								<article>
-									<header>
-										<span class="date">April 14, 2017</span>
-										<h2><a href="#">Tempus sed<br />
-										nulla imperdiet</a></h2>
-									</header>
-									<a href="#" class="image fit"><img src="images/pic05.jpg" alt="" /></a>
-									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Full Story</a></li>
-									</ul>
-								</article>
-								<article>
-									<header>
-										<span class="date">April 11, 2017</span>
-										<h2><a href="#">Odio magna<br />
-										sed consectetur</a></h2>
-									</header>
-									<a href="#" class="image fit"><img src="images/pic06.jpg" alt="" /></a>
-									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Full Story</a></li>
-									</ul>
-								</article>
-								<article>
-									<header>
-										<span class="date">April 7, 2017</span>
-										<h2><a href="#">Augue lorem<br />
-										primis vestibulum</a></h2>
-									</header>
-									<a href="#" class="image fit"><img src="images/pic07.jpg" alt="" /></a>
-									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Full Story</a></li>
-									</ul>
-								</article>
+										<header>
+											<span class="date">Match du jour</span>
+											<br>
+											<br>
+											<h1>France vs Pérou</h1>
+										</header>
+										<a href="#" class="image fit"><img src="images/equipe2.jpg" alt="" /></a>
+										
+										<p class="buttoncenter">Pariez sur ce match ? </p>
+										
+										<br>
+										<ul class="actions">
+											<form >
+												
+													<label for="equipe1" class="col-md-4 control-label">Score Equipe 1 </label>
+	
+													<br>
+												<input type="text" name="equipe1" placeholder=" Entrez le score de l'equipe 1 "> 
+												
+	
+											</br>
+										</br>
+	
+												<label for="equipe1" class="col-md-4 control-label">Score Equipe 2 </label>
+	
+												<input type="text" name="equipe1" placeholder=" Entrez le score de l'equipe 2 "> 
+	
+												<div class="col-xs-6">
+														<div class="text-center"><button class="btn btn-primary btn-sx" type="button">Confirm</button></div>
+													</div>
+											</form>
+	
+										</ul>
+									</article>
 							</section>
 
 
