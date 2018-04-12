44c29aeb2ee44a0b5f5728d4a084e53170a7c1df
younesbenmammar <younescomptepro@gmail.com>
2018-04-12 16:08:32 +0200

    Inscription button

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -101,7 +101,7 @@
             <div class="form-group">
                 <div class="col-md-6 col-md-offset-4">
                     <button type="submit" class="btn btn-primary">
-                        Register
+                        S'inscrire
                     </button>
                 </div>
             </div>
